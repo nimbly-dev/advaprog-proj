baa783b50497a5d3fa8dfb28ba6c6d10410902db
alexcordero <s.xanarius@gmail.com>
2020-06-05 16:10:45 +0800

    with verification pero hindi password_veify hindi ko mapagana

---
+++ services/processes/process-confirm-checkout.php
@@ -0,0 +1,59 @@
+<?php
+
+  //
+  include ('../services/DB_Operations.php');
+?>
+
+<!-- To be revised depending on circumstances -->
+
+
+<?php
+//SQL STATEMENT FOR SELECTING ALL INPUTS ON "product_list" table
+$sql = "SELECT * FROM product_list";
+// puts it on the result var and connects the sql query to the db
+$result = $conn->query($sql);
+
+
+$resultCount =  $result->num_rows;
+
+// Sets the value of the imgEnlarge depending what prouct was clicked
+
+
+  echo "<p>Hello ".$_SESSION['username']." <br></p>";
+  echo "<p>Your order is as follows</p>";
+
+
+  // Iterate throughout the whole product List
+  for ($ctr = 0; $ctr < $resultCount; $ctr++){
+    $row = $result -> fetch_assoc();
+?>
+<div class="container">
+   <?php  if($row['productID'] == $_SESSION['selectedProductBuy']){ ?>
+       <?php echo "<h2>{$row['productName']}</h2>"; ?>
+       <?php echo "<img src='{$row['imgPath']}' class='singleProductImg'>" ;  ?>
+       <?php echo "Product Price: {$row['productPrice']}"; ?>
+     <br><br>
+   <?php  } ?>
+
+<?php } ?>
+<!-- Shipping and payment print -->
+<?php
+  $address = $_POST['addr'];
+  $method = $_POST['paymentMethod'];
+  echo "<p>Shipping Address: ".$address."</p>";
+  echo "<p>Payment Method: ".$method."</p>";
+ ?>
+<!-- Buy Button -->
+<?php
+ echo "<form method='POST' action=''>";
+ echo "<button type='submit' class='btn btn-success'>Buy</button>";
+ echo "</form>";
+ ?>
+ <!-- Return Button -->
+ <br>
+<?php
+ echo "<a href= '../website/browse.php'>";
+ echo "<button type='button' class='btn btn-danger'>Cancel</button>";
+ echo "</a>";
+ ?>
+</div>
---
+++ website/login.php
@@ -0,0 +1,55 @@
+<?php
+
+  include ("../services/processes/process-login.php");
+
+ ?>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<link rel="stylesheet" type="text/css" href="../css/registration/registration.css">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+		<link rel="stylesheet" type="text/css" href="../css/registration/registration.css">
+	</head>
+	<body>
+
+		<!-- -->
+	<div class="container">
+
+            <!-- INCLUDE THE ERROR PHP CLASS -->
+						<form method="POST" action="login.php">
+							<div class="header">
+								<h2>LOGIN</h2>
+							</div>
+              <!-- Calls the crendital_validators here -->
+              <?php  include ('../services/handlers/credential_validators.php') ?>
+							<div class="input-group">
+								<label>Username:</label>
+								<input type="text" name="username" placeholder="Enter username " id="username"></input>
+							</div>
+							<div class="input-group">
+								<label>Password:</label>
+								<input type="text" name="password" id="password"  placeholder="Enter password"></input>
+							</div>
+							<div>
+								<input type="submit" value="Login" class="btn" name="login"></input>
+							</div>
+              <div>
+                <a href="register.php"> Not a member? </a>
+                <a href="Landing-page.php"> Home </a>
+              </div>
+
+
+
+						</form>
+
+    </div>
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
+
+</html>
