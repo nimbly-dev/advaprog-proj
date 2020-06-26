b9c37b8f36b2de362c141d28aa913c162cf63f5f
nimbly-dev <nimbly2016@gmail.com>
2020-06-26 15:20:38 +0800

    Added remember address checkbox function from checkout form

--- services/processes/process-payment-COD.php
+++ services/processes/process-payment-COD.php
@@ -10,9 +10,18 @@
 
  <form action="../website/confirm-checkout.php?" method="POST">
    <!-- Text input box for delivery Address -->
-   <div class="form-group">
-     <label for="formGroupExampleInput">Delivery Address</label>
-     <input type="text" class="form-control" name="address" id="address"  placeholder="Enter Address" required>
+   <div class="d-inline">
+     <div class="form-group">
+       <label for="formGroupExampleInput">Delivery Address</label>
+       <input type="text" class="form-control" name="address" id="address"  placeholder="Enter Address"
+        value="<?php echo "".@$_SESSION['address']; ?>" required>
+     </div>
+     <div class="custom-control custom-checkbox">
+       <input type="checkbox" class="custom-control-input" id="rememberAddress" name="rememberAddress"
+        <?php if(@$_SESSION['address'] != null){echo "checked";}?>>
+       <label class="custom-control-label" for="rememberAddress">
+         Remember Address</label>
+     </div>
    </div>
    <br>
    <!-- CREATION OF COD ID WOAH -->
